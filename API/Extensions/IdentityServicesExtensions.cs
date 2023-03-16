0ef2c4704b33133a924a29dac52ed2a9b1881c4d
krushnai <krushnai.nirgude@ecotechservices.com>
2023-03-16 12:56:53 +0530

    End of Section4

--- API/Extensions/IdentityServicesExtensions.cs
+++ API/Extensions/IdentityServicesExtensions.cs
@@ -1,31 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
 {
-    public class IdentityServicesExtensions
+    public  static class IdentityServicesExtensions
     {
         public static IServiceCollection AddIdentityServices
         (this IServiceCollection services, IConfiguration config)
         {
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(Options =>
+            .AddJwtBearer(Options =>
     {
         Options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuerSigningKey = true,
             IssuerSigningKey = new SymmetricSecurityKey
-(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+            (Encoding.UTF8.GetBytes(config["TokenKey"])),
             ValidateIssuer = false,
             ValidateAudience = false
         };
-
+            
     });
-
+return services;
         }
     }
 }
